fea37261be5fa35876b18261e5636ab7d59ee8d7
IShahnawazShaikh <shahnawazexplore@gmail.com>
2021-12-26 21:30:20 +0530

    first commit

---
+++ src/main/java/com/swar/yantra/SwarYantraApplication.java
@@ -0,0 +1,13 @@
+package com.swar.yantra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwarYantraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwarYantraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swar/yantra/SwarYantraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swar.yantra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwarYantraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
